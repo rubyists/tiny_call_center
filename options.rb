ea19c955519e733279185a4fc54c640a7dab62c2
The Rubyists, LLC <rubyists@rubyists.com>
2011-02-15 08:42:26 -0600

    Change env variables to TCC_*

--- options.rb
+++ options.rb
@@ -6,32 +6,32 @@ module TinyCallCenter
   options.dsl do
     sub :listener do
       o "WebSocket server URI for listeners", :server,
-        URI(ENV["TinyCallCenter_WebSocketListenerURI"] || 'ws://127.0.0.1:8081/websocket')
+        URI(ENV["TCC_WebSocketListenerURI"] || 'ws://127.0.0.1:8081/websocket')
     end
 
     sub :ribbon do
       o "WebSocket server URI for agents", :server,
-        URI(ENV["TinyCallCenter_WebSocketAgentURI"] || 'ws://127.0.0.1:8080/websocket')
+        URI(ENV["TCC_WebSocketAgentURI"] || 'ws://127.0.0.1:8080/websocket')
     end
 
     o "FreeSWITCH Command Server", :command_server,
-      ENV["TinyCallCenter_Server"] || '127.0.0.1'
+      ENV["TCC_Server"] || '127.0.0.1'
 
-    o "FreeSWITCH Command Server", :fs_port,
-      ENV["TinyCallCenter_FsPort"] || 8021
+    o "FreeSWITCH Command Server Port", :fs_port,
+      ENV["TCC_FsPort"] || 8021
 
-    o "FreeSWITCH Command Server", :fs_auth,
-      ENV["TinyCallCenter_FsAuth"] || "ClueCon"
+    o "FreeSWITCH Command Server Authentication", :fs_auth,
+      ENV["TCC_FsAuth"] || "ClueCon"
 
     o "FreeSWITCH Registration Servers For Agents", :registration_servers,
-      (ENV["TinyCallCenter_Monitors"] ? ENV["TinyCallCenter_Monitors"].split(":") : ['127.0.0.1'])
+      (ENV["TCC_Monitors"] ? ENV["TCC_Monitors"].split(":") : ['127.0.0.1'])
 
     o "Agents Off-Hook instead of On-Hook", :off_hook,
-      ENV["TinyCallCenter_OffHook"] || false
+      ENV["TCC_OffHook"] || false
 
     o "Sqlite Database File", :db,
-      ENV["TinyCallCenter_DB"] || "sqlite://%s" % File.expand_path("../db/call_center.db", __FILE__)
+      ENV["TCC_DB"] || "sqlite://%s" % File.expand_path("../db/call_center.db", __FILE__)
 
-    o "Accounts Backend", :backend, ENV["TinyCallCenter_Backend"]
+    o "Accounts Backend", :backend, ENV["TCC_Backend"]
   end
 end
