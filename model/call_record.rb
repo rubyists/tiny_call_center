f8df8d7f76e40a484c5bb447d52f42cc30c7499c
Tj Vanderpoel <tj@rubyists.com>
2011-02-25 11:53:20 -0600

    refactored model to return dataset

--- model/call_record.rb
+++ model/call_record.rb
@@ -11,7 +11,11 @@ module TinyCallCenter
     def self.agent_history(agent, from = Date.today, to = nil)
       ds = filter{{:agent => agent} & (created_at > from)}
       ds = filter{(created_at < to)} if to
-      ds.order_by(:created_at.desc).map(&:values)
+      ds.order_by(:created_at.desc)
+    end
+
+    def self.agent_history_a(agent, from = Date.today, to = nil)
+      agent_history(agent, from, to).map(&:values)
     end
 
     def validate
