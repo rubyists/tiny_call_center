cba24ad4ae1da5dcb85d789bbc53734edde35aa7
Michael Fellinger <mf@rubyists.com>
2011-12-11 12:34:01 -0600

    Make memcached caching work again for bin/fs2ws

--- lib/tiny_call_center/utils/memcached_backend.rb
+++ lib/tiny_call_center/utils/memcached_backend.rb
@@ -1,13 +1,20 @@
 module TinyCallCenter
   module MemcachedBackend
     def initialize(*args, &block)
-      require "memcached"
+      begin
+      require "memcache"
+      rescue LoadError => ex
+        p ex
+      end
+
       @originate_cache = MemCache.new(TCC.options.memcached.servers, prefix_key: 'orig_')
       @answer_cache = MemCache.new(TCC.options.memcached.servers, prefix_key: 'answer_')
+
       super
     rescue => ex
+      puts ex, ex.backtrace
       Log.error(ex)
-      exit
+      abort "Error in memcached: #{ex}"
     end
 
     def set_originate(uuid, msg)
@@ -15,7 +22,7 @@ module TinyCallCenter
     end
 
     def get_originate(uuid)
-      @originate_cache.get("#{uuid}") rescue nil
+      @originate_cache.get("#{uuid}", nil) rescue nil
     end
 
     def set_answer(uuid, msg)
@@ -23,9 +30,7 @@ module TinyCallCenter
     end
 
     def get_answer(uuid)
-      @answer_cache.get("#{uuid}") rescue nil
+      @answer_cache.get("#{uuid}", nil) rescue nil
     end
   end
 end
-
-
