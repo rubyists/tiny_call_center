a940daf9d716c8e4e5e81270b1462af4d741b8f7
rubyists <rubyists@rubyists.com>
2012-01-24 20:31:11 -0600

    split up channel messages better, now sending new_call, update_call, and end_call

--- lib/tiny_call_center/jack_tube.rb
+++ lib/tiny_call_center/jack_tube.rb
@@ -1,36 +1,67 @@
 require "em-jack"
 require "json"
+
 module TCC
   class JackTube < EMJack::Connection
-    def process_job(ribbon, channel, json)
-      case channel.to_s
-      when /insert/
-        ribbon.backbone_create(channel, json)
-      when /update/
-        ribbon.backbone_update(channel, json)
-      when /delete/
-        ribbon.backbone_delete(channel, json)
-      end
-    end
+    def watch_socket(tube_names)
+      tube_names.each{|tube_name| watch(tube_name) }
 
-    def process_jobs(ribbon)
       each_job do |job|
-        channel, json = job.body.split("\t",2)
-        json = JSON.parse json
-        delete(job).callback do
-          process_job(ribbon, channel, json)
-          TCC::Log.debug "deleted #{{channel => json}}"
-        end
+        Log.debug job: job
+        delete(job) if handle_job(job)
       end
     end
 
-    def watch_socket(tube, ribbon)
-      watch(tube).callback do |_tube|
-        TCC::Log.info "Jack is listening on #{_tube}"
-        process_jobs(ribbon)
+    def handle_job(job)
+      channel, payload, * = job.body.split("\t")
+      type, action = channel.to_s.split("_",2)
+      # action is update, insert, or delete
+      # type is agent, channel, call, etc.
+      if type == 'channel'
+        if action == 'update'
+          last_state, json = payload.split(":",2)
+          body = JSON.parse(json)
+          uuid = body.delete("uuid")
+          channel_update uuid, last_state, body
+        else
+          body = JSON.parse(payload)
+          uuid = body.delete("uuid")
+          __send__ channel, uuid, body
+        end
+      else
+        body = JSON.parse(payload)
+        RibbonAgent.__send__("pg_#{type}", action, body)
       end
+      true
+    rescue => ex
+      Log.error ex
+      false
+    end
+
+    def channel_insert(uuid, body)
+      cid_num, dest = body.values_at('cid_num', 'dest')
+      return false if cid_num.nil? && dest.nil?
+      Log.debug "New call #{uuid}: #{body}"
+      RibbonAgent.__send__("new_call", uuid, cid_num, dest, body)
+    end
+
+    def channel_update(uuid, last_state, body)
+      new_state = body["callstate"]
+      Log.debug "Callstate Changed for #{uuid}: #{last_state} => #{new_state}"
+      cid_num, dest = body.values_at('cid_num', 'dest')
+      return false if cid_num.nil? && dest.nil?
+      if ['RINGING', 'DOWN'].include?(last_state) && new_state == 'ACTIVE'
+        # This is a new call
+        RibbonAgent.__send__("new_call", uuid, cid_num, dest, body)
+      else
+        # Just an update to a call
+        RibbonAgent.__send__("update_call", uuid, cid_num, dest, last_state, body)
+      end
+    end
+
+    def channel_delete(uuid, body)
+      Log.debug "Call Ended #{uuid}: #{body}"
+      RibbonAgent.__send__("end_call", uuid, body)
     end
   end
 end
-
-
